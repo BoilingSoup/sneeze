c0a7cc92aafeac625a070d366cfc70ab2d2c91eb
BoilingSoup <boilingsoup@protonmail.com>
2025-08-05 16:55:35 -0700

    update config

--- stubs/api/config/sneeze.php
+++ stubs/api/config/sneeze.php
@@ -1,21 +1,20 @@
 <?php
 
 return [
-    // Function that returns the sanctum auth token's expiration time.
-    // ex. When User registers/logs in, their token is valid for 1 month.
+
+    // Set how long Sanctum auth tokens are valid after login or registration
     'sanctum_auth_token_expiration_fn' => function () {
         return now()->addMonths(1);
     },
 
-    // Function that returns the email verification code's expiration time.
-    // ex. When User requests an email verification code, the verification code is valid for 15 minutes.
+    // Set how long email verification codes are valid
     'email_verification_expiration_fn' => function () {
         return now()->addMinutes(15);
     },
 
-    // Function that returns the password reset code's expiration time.
-    // ex. When User requests a password reset code, the reset code is valid for 15 minutes.
+    // Set how long password reset codes are valid
     'password_reset_expiration_fn' => function () {
         return now()->addMinutes(15);
     }
+
 ];
